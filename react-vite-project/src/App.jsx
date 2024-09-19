f8e58eb9844b9aa6a50c11ee73f2376c4889d084
shadow6444 <shadowgarden644@gmail.com>
2024-09-19 12:43:18 +0500

    Setting up project

--- react-vite-project/src/App.jsx
+++ react-vite-project/src/App.jsx
@@ -1,27 +1,34 @@
-import Header from "./Header.jsx";
-import List from "./List.jsx";
-import Footer from "./Footer.jsx";
 import "./App.css";
+import Main from "./components/Main.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
-  /**
-Challenge: 
+/**
+Challenge: Project setup
 
-- Add an `ul` inside the Header's `nav` and create
-  the following `li`s: "Pricing", "About", & "Contact"
-- Using flexbox, line up the nav items horizontally, and
-  put them inline with the React logo.
-- Change the image styling to happen in CSS instead of in-line
-  For practice, add a new class to the image in order to style it
-- Challenge: move the Footer and MainContent components
-  into their own files.
+- Create a `components` folder
+- Create the following components in separate files inside
+  the components folder.  In each one, just render an `h1` 
+  with the name of the component (e.g. return <h1>Navbar goes here</h1>):
+    - Navbar
+    - Main
+- Create an App component outside the components folder (sibling to
+  the index.js file)
+    - Have App render the Navbar and Main components
+- Import and render the App component inside of index.js using ReactDOM
+    - At this point you should have your "Navbar goes here" etc. showing up
+      in the mini-browser.
+- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+  Put the links to those fonts ABOVE the style.css link in index.html (Use
+  the `<link/>` elements instead of the import or npm options for getting
+  the fonts. You may need to do some extra research to figure out how this 
+  works if you haven't done it before)
 */
 
   return (
     <>
-      <Header />
-      <List />
-      <Footer />
+      <Navbar/>
+      <Main/>
     </>
   );
 }
