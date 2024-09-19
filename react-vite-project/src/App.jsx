64462a9c6f43d7e9177732d6aab8ad9205d426e5
shadow6444 <shadowgarden644@gmail.com>
2024-09-19 09:57:39 +0500

    Added Header component

--- react-vite-project/src/App.jsx
+++ react-vite-project/src/App.jsx
@@ -1,24 +1,22 @@
 function App() {
   /**
-Challenge: 
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
 
-Part 2: 
-- Add a `header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a `footer` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
- */
-  return (
-    <>
+  function Header() {
+    return (
       <header>
         <nav>
           <img src="./react-logo.png" alt="react logo" width="50px" />
         </nav>
       </header>
+    );
+  }
+  return (
+    <>
+      <Header />
       <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>
