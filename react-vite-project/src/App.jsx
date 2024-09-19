684eb71530957fee55cc7631e9ab0b56cc80e33d
shadow6444 <shadowgarden644@gmail.com>
2024-09-19 09:54:37 +0500

    Completed a challenge

--- react-vite-project/src/App.jsx
+++ react-vite-project/src/App.jsx
@@ -1,12 +1,32 @@
 function App() {
+  /**
+Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
   return (
     <>
-    <img src="./react-logo.png" alt="react logo" width="40px"/>
-      <h1>Reasons why i love react</h1>
-      <ul>
-        <li>React is very easy to learn and use.</li>
-        <li>React enhances the concepts of reusability and composibilty.</li>
-      </ul>
+      <header>
+        <nav>
+          <img src="./react-logo.png" alt="react logo" width="50px" />
+        </nav>
+      </header>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+      <footer>&copy; 2024 React development. All rights reserved.</footer>
     </>
   );
 }
