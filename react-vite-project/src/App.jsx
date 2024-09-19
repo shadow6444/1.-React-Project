dc5d7858949617cb8e9513f8dd1c3ca3f63d00b4
shadow6444 <shadowgarden644@gmail.com>
2024-09-19 08:48:52 +0500

    React Practice

---
+++ react-vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+    <img src="./react-logo.png" alt="react logo" width="40px"/>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
