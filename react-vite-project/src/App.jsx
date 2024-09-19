05874a824c0eb93de35073413a26889bd56a1bfc
shadow6444 <shadowgarden644@gmail.com>
2024-09-19 10:02:53 +0500

    Added Composability

--- react-vite-project/src/App.jsx
+++ react-vite-project/src/App.jsx
@@ -1,9 +1,13 @@
 function App() {
-  /**
-Mini Challenge:
-Move the `header` element from Page into 
-its own component called "Header"
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
 */
+
 
   function Header() {
     return (
@@ -14,17 +18,28 @@ its own component called "Header"
       </header>
     );
   }
+  function List() {
+    return (
+      <>
+        <h1>Reasons I'm excited to learn React</h1>
+        <ol>
+          <li>
+            It's a popular library, so I'll be able to fit in with the cool
+            kids!
+          </li>
+          <li>I'm more likely to get a job as a developer if I know React</li>
+        </ol>
+      </>
+    );
+  }
+  function Footer() {
+    return <footer>&copy; 2024 React development. All rights reserved.</footer>;
+  }
   return (
     <>
       <Header />
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>
-          It's a popular library, so I'll be able to fit in with the cool kids!
-        </li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-      </ol>
-      <footer>&copy; 2024 React development. All rights reserved.</footer>
+      <List />
+      <Footer />
     </>
   );
 }
